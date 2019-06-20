93f7d476d1470cd0661dfeb8208bf5384016117f
Rebeacca Modayil <rebeacca.modayil@temple.edu>
2019-06-20 12:54:47 -0400

    countdown

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,42 @@
-var questions = [{
-    q: "What year did The Simpsons start airing?"
-    options: 1969, 1978, 1989, 1991
-    a: 1989
-}, {
-    q: "What is the name of the Simpson's cat?"
-    options: "Santa's Little Helper", "Snowball", "Moe", "The cat doesn't have a name"
-    a: "Snowball"
-}, {
-    q: "Who founded the Simpson's town?"
-    options: "Zachariah Springfield", "Louis Springfield", "Jebadiah Springfield", "William Springfield"
-    a: "Jebadiah Springfield"
-}, {
-    q: "Hold old is Bart?"
-    options: 10, 12, 8, 9
-    a: 10 
-}, {
-    q: "Who is Mr. Burns' assistant?"
-    options: "Seymour Skinner", "Homer Simpson", "Waylon Skinner", "Slideshow Bob"
-    a: "Waylon Skinner"
-}, {
-    q: "As of 2019, how many seasons are there?"
-    options: 23, 46, 50, 30
-    a: 30
-}, {
-    q: "Which of the following is NOT one of Homer's sayings?"
-    options: "D'oh!", "Why you little...!!", "Mmmmm Donuts", "Okily Dokily!"
-    a: "Okily Dokily!"
+// var questions = [{
+//     q: "What year did The Simpsons start airing?"
+//     options: 1969, 1978, 1989, 1991
+//     a: 1989
+// }, {
+//     q: "What is the name of the Simpson's cat?"
+//     options: "Santa's Little Helper", "Snowball", "Moe", "The cat doesn't have a name"
+//     a: "Snowball"
+// }, {
+//     q: "Who founded the Simpson's town?"
+//     options: "Zachariah Springfield", "Louis Springfield", "Jebadiah Springfield", "William Springfield"
+//     a: "Jebadiah Springfield"
+// }, {
+//     q: "Hold old is Bart?"
+//     options: 10, 12, 8, 9
+//     a: 10 
+// }, {
+//     q: "Who is Mr. Burns' assistant?"
+//     options: "Seymour Skinner", "Homer Simpson", "Waylon Skinner", "Slideshow Bob"
+//     a: "Waylon Skinner"
+// }, {
+//     q: "As of 2019, how many seasons are there?"
+//     options: 23, 46, 50, 30
+//     a: 30
+// }, {
+//     q: "Which of the following is NOT one of Homer's sayings?"
+//     options: "D'oh!", "Why you little...!!", "Mmmmm Donuts", "Okily Dokily!"
+//     a: "Okily Dokily!"
 
-}];
+// }];
 
-var timer = 30;
-var reset;
-var incorrect = 0;
-var correct = 0;
+var timeleft = 30;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval(downloadTimer);
+    document.getElementById("countdown").innerHTML = "Times Up!"
+  }
+}, 1000);
+
 
